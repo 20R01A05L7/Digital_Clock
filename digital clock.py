dd652085aa57540ff482c57e1e5936b68a1c2fa2
20R01A05L7 <111648825+20R01A05L7@users.noreply.github.com>
2023-04-08 19:12:05 +0530

    Update digital clock.py

--- digital clock.py
+++ digital clock.py
@@ -7,7 +7,7 @@ def present_time():
     display_time = time.strftime("%I:%M:%S %p")
     d_clock.config(text=display_time)
     d_clock.after(200,present_time)
-d_clock = Label(root, font=("arial",80),background="black",foreground="white")
+d_clock = Label(root, font=("times",80),background="black",foreground="white")
 d_clock.pack(anchor="center")
 present_time()
 root.mainloop()
